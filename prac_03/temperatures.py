4e2023272a1ef6b918622d02b7fe1aaf93224ab2
JoshuaGale <joshua.gale@my.jcu.edu.au>
2017-08-16 15:26:51 +1000

    Add temperatures program with functions

---
+++ prac_03/temperatures.py
@@ -0,0 +1,37 @@
+MENU = """C - Convert Celsius to Fahrenheit
+
+F - Convert Fahrenheit to Celsius
+
+Q - Quit"""
+
+
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            convert_celsius_to_fahrenheit()
+
+        elif choice == "F":
+            convert_fahrenheit_to_celsius()
+
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+
+    print("Thank you.")
+
+
+def convert_fahrenheit_to_celsius():
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    print("Result: {:.2f} C".format(celsius))
+
+
+def convert_celsius_to_fahrenheit():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print("Result: {:.2f} F".format(fahrenheit))
+
+main()
