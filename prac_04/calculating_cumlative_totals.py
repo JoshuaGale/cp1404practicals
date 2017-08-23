ee3143273a02e65bdebdc2691b34449e2b66ea69
JoshuaGale <joshua.gale@my.jcu.edu.au>
2017-08-23 13:59:23 +1000

    Compleate all excercise requirements

--- prac_04/calculating_cumlative_totals.py
+++ prac_04/calculating_cumlative_totals.py
@@ -12,18 +12,20 @@ def main():
 
     incomes = []
 
-    months = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, number_of_months + 1):
+        income = float(input("Enter income for month {}: ".format(month)))
 
         incomes.append(income)
 
+    display_income_report(incomes, number_of_months)
+
+
+def display_income_report(incomes, number_of_months):
     print("\nIncome Report\n-------------")
-
     total = 0
-
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
 
         total += income
--- prac_04/warm_up.py
+++ prac_04/warm_up.py
@@ -1,11 +1,14 @@
-numbers = [3, 1, 4, 1, 5, 9, 2]
+def main():
+    numbers = [3, 1, 4, 1, 5, 9, 2]
 
-numbers[0] = "ten"
+    numbers[0] = "ten"
 
-numbers[-1] = 1
+    numbers[-1] = 1
 
-print(numbers)
+    print(numbers)
 
-print(numbers[2:])
+    print(numbers[2:])
 
-print(9 in numbers)
+    print(9 in numbers)
+
+main()
