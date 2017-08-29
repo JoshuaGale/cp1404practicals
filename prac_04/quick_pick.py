6fe59c500d6c9a46ce5df549d9cd8b58b1f9ae64
JoshuaGale <joshua.gale@my.jcu.edu.au>
2017-08-29 15:27:29 +1000

    Add quick_pick code

--- prac_04/quick_pick.py
+++ prac_04/quick_pick.py
@@ -1,13 +1,23 @@
 import random
+MINIMUM = 1
+MAXIMUM = 45
 
 
 def main():
-    quick_picks = []
     number_of_quick_picks = int(input("how many quick picks? "))
-    
+    while number_of_quick_picks < 0:
+        print("please enter a number greater than 0")
+        number_of_quick_picks = int(input("how many quick picks? "))
 
-
-
+    for i in range(number_of_quick_picks):
+        quick_pick = []
+        for j in range(number_of_quick_picks):
+            number = random.randint(MINIMUM, MAXIMUM)
+            while number in quick_pick:
+                number = random.randint(MINIMUM, MAXIMUM)
+            quick_pick.append(number)
+        quick_pick.sort()
+        print(" ".join("{:2}".format(number) for number in quick_pick))
 
 
 main()
