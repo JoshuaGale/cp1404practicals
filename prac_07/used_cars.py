ec4cb7275af190122637bb2ff79290a2a74792af
JoshuaGale <joshua.gale@my.jcu.edu.au>
2017-09-20 12:04:43 +1000

    Add car and used_car code

---
+++ prac_07/used_cars.py
@@ -0,0 +1,11 @@
+from prac_07.car import Car
+
+
+def main():
+    my_car = Car(180)
+    my_car.drive(30)
+    print("fuel =", my_car.fuel)
+    print("odo =", my_car.odometer)
+    print(my_car)
+
+main()
