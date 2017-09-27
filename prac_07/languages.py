4b6bd0bbc742e379de268bbb3217cda1dfd0e84d
jc463121 <joshua.gale@my.jcu.edu.au>
2017-09-27 12:56:26 +1000

    Add run_tests function for languages program

--- prac_07/languages.py
+++ prac_07/languages.py
@@ -1,10 +1,15 @@
 from prac_07.programming_language import ProgrammingLanguage
 
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-print(ruby)
-languages = [ruby, python, visual_basic]
-print("the dynamic language types are")
-[print(language.name) for language in languages if language.is_dynamic()]
 
+def run_tests():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(ruby)
+    languages = [ruby, python, visual_basic]
+    print("the dynamic language types are")
+    [print(language.name) for language in languages if language.is_dynamic()]
+
+
+if __name__ == "__main__":
+    run_tests()
