d5ea9656ffa0ca30f94e051cef2f2d691d8d780f
JoshuaGale <joshua.gale@my.jcu.edu.au>
2017-10-04 11:28:49 +1000

    Add unreliable_car and unreliable_car_test files

---
+++ prac_08/unreliable_car.py
@@ -0,0 +1,16 @@
+from prac_08.car import Car
+import random
+
+
+class UnreliableCar(Car):
+
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        breakdown_chance = random.randint(0, 100)
+        if breakdown_chance > self.reliability:
+            return 0
+        super().drive(distance)
+
---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,9 @@
+from prac_08.unreliable_car import UnreliableCar
+
+car_1 = UnreliableCar("car 1", 100, 50)
+print(car_1)
+car_1.drive(20)
+print(car_1)
+car_1.add_fuel(80)
+print(car_1)
+
