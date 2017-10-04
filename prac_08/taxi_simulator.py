7c501663f45e151f8803e9564041f77881a55815
jc463121 <joshua.gale@my.jcu.edu.au>
2017-10-04 14:40:53 +1000

    Fix taxi_simulator code to be more efficient

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -1,31 +1,42 @@
 from prac_08.taxi import Taxi
 from prac_08.silver_service_taxi import SilverServiceTaxi
+MENU = "q)uit, c)hoose taxi, d)rive"
 
-taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
-bill_to_date = 0
 
-print("Let's Drive!")
-menu_choice = (input("q)uit, c)hoose taxi, d)rive\n>>> ")).lower()
-while menu_choice != "q":
-    if menu_choice == "c":
-        print("Taxis available:")
-        print("0 - {}\n1 - {}\n2 - {}".format(taxis[0], taxis[1], taxis[2]))
-        taxi_choice = int(input("Choose taxi: "))
+def main():
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    bill_to_date = 0
+
+    print("Let's Drive!")
+    print(MENU)
+    menu_choice = (input(">>> ")).lower()
+    while menu_choice != "q":
+        if menu_choice == "c":
+            print("Taxis available:")
+            display_taxi(taxis)
+            taxi_choice = int(input("Choose taxi: "))
+            current_taxi = taxis[taxi_choice]
+
+        elif menu_choice == "d":
+            current_taxi.start_fare()
+            drive_distance = int(input("Drive how far? "))
+            current_taxi.drive(drive_distance)
+            print("your {} trip cost you ${:.2f}".format(current_taxi.name, current_taxi.get_fare()))
+            bill_to_date += current_taxi.get_fare()
+
+        else:
+            print("invalid menu choice")
         print("Bill to date: ${:.2f}".format(bill_to_date))
+        print(MENU)
+        menu_choice = (input(">>> ")).lower()
 
-    elif menu_choice == "d":
-        taxis[taxi_choice].start_fare()
-        drive_distance = int(input("Drive how far? "))
-        taxis[taxi_choice].drive(drive_distance)
-        print("your {} trip cost you ${:.2f}".format(taxis[taxi_choice].name, taxis[taxi_choice].get_fare()))
-        bill_to_date += taxis[taxi_choice].get_fare()
-        print("Bill to date: ${:.2f}".format(bill_to_date))
+    print("Total trip cost: ${:.2f}".format(bill_to_date))
+    print("Taxis are now:")
+    display_taxi(taxis)
 
-    else:
-        print("invalid menu choice")
 
-    menu_choice = (input("q)uit, c)hoose taxi, d)rive\n>>>")).lower()
+def display_taxi(taxis):
+    for i, taxi in enumerate(taxis):
+        print("{} - {}".format(i, str(taxi)))
 
-print("Total trip cost: ${:.2f}".format(bill_to_date))
-print("Taxis are now:")
-print("0 - {}\n1 - {}\n2 - {}".format(taxis[0], taxis[1], taxis[2]))
+main()
